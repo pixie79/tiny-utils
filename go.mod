b4f998539c596dadaebef4b7b7b7d827ac2c75f1
Mark Olliver <mark@olliver.me.uk>
2023-09-11 15:23:24 +0100

    move net code to own file

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pixie79/tiny-utils
+
+go 1.20
--- utils/utils.go
+++ utils/utils.go
@@ -13,7 +13,6 @@ import (
 	"fmt"
 	"io"
 	"math/rand"
-	"net/http"
 	"os"
 	"sort"
 	"strings"
@@ -92,41 +91,6 @@ func LinesFromReader(r io.Reader) []string {
 	MaybeDie(err, "could not parse lines")
 
 	return lines
-}
-
-// UrlToLines retrieves the contents of a URL and returns them line by line.
-//
-// Parameters:
-// - url: the URL to retrieve the contents from.
-// - username: the username for basic authentication. If not needed, leave it empty.
-// - password: the password for basic authentication. If not needed, leave it empty.
-//
-// Returns:
-// - lines: an array of strings containing the lines of the retrieved content.
-func UrlToLines(url string, username string, password string) []string {
-	client := http.Client{Timeout: 5 * time.Second}
-
-	req, err := http.NewRequest(http.MethodGet, url, http.NoBody)
-	MaybeDie(err, "could not create http request")
-
-	// Add basic auth if username and password are set
-	if username != "" && password != "" {
-		req.SetBasicAuth(username, password)
-	}
-
-	res, err := client.Do(req)
-	MaybeDie(err, "could not authenticate")
-
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		MaybeDie(err, "error closing connection")
-	}(res.Body)
-
-	if !InBetween(res.StatusCode, 200, 299) {
-		Die(fmt.Sprintf("url access error %s, Status Code: %d", url, res.StatusCode))
-	}
-
-	return LinesFromReader(res.Body)
 }
 
 // InBetween checks if a number is within a given range.
